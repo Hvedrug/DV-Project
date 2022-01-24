5eeffc810490e00395e2149e3e27912a84006f29
Hvedrug <63800678+Hvedrug@users.noreply.github.com>
2022-01-24 18:41:15 +0100

    Update dv-project.js
    graph 3 working

--- dv-project.js
+++ dv-project.js
@@ -8,10 +8,11 @@ const countriesEurope = ['Albania', 'Austria', 'Belarus', 'Belgium', 'Bosnia and
 const governmentPolicies = ["testing_policy","vaccination_policy","facial_coverings","income_support","public_information_campaigns","cancel_public_events","close_public_transport","school_closures","stay_home_requirements"];
 const covidFactors = ["Daily new Covid cases", "Daily number of death", "Number of people in public spaces", "Number of people in public transport", "Number of people in work places", "Number of people in grocery stores", "Number of people in non essential stores"]
 var parameter = {country: 11, policy: 2, factor: 0};
-var DebugData;
-var DictData=[];
+var dictData=[];
 var XScale;
 var YScale;
+var startDate = ["2020-06-22","2020-05-11","2020-03-17", "2020-10-17"];
+var extractedData = [];
 
 // append the svg object to the body of the page
 const svg = d3.select("#third-graph")
@@ -21,18 +22,31 @@ const svg = d3.select("#third-graph")
 .attr("transform", `translate(${margin.left},${margin.top})`);
 
 
+
+
+
+
+
 // load the first set of data when load page
 window.onload = function() {
   initSelect(countriesEurope, "selectCountry", parameter["country"]);
   initSelect(governmentPolicies, "selectPolicy", parameter["policy"]);
   initSelect(covidFactors, "selectFactor", parameter["factor"]);
-  initPage('data/new_cases_per_million.csv');
+  initSelect(countriesEurope, "selectGraph3", parameter["country"]);
+  initPage('data/new_deaths_per_million.csv');
 };
- 
+  
+
+
+
+
+
+
+
 
 function initPage(src){
   for (i=0; i<countriesEurope.length; i++){
-    DictData[i] = [];
+    dictData[i] = [];
   }
   // Add X axis 
   svg.append("g")
@@ -41,16 +55,86 @@ function initPage(src){
   // Add Y axis
   svg.append("g")
   .attr("class", "y axis");
+  svg.append("g")
+  .attr("class", "rect");
 
-  getData(src).then(function(){
-    updateSelect();
-    updateGraph();
+  getData(src, dictData).then(function(){
+    extractedData = extractData(startDate, dictData[parameter['country']], 'red', extractedData);
+    updateALL(extractedData);
   });
 }
 
-// function that get the data and parse it
-function getData(src){
-  return d3.csv(src,
+
+function updateALL(graphData) {
+  
+  // maj X axis 
+  XScale = d3.scaleLinear()
+  .domain([0, (startDate.length * 30)])
+  .range([ 0, widthReduced ]);
+  svg.selectAll("g.x.axis")
+  .attr("transform", `translate(0, ${heightReduced})`)
+  .call(d3.axisBottom(XScale));
+
+  // maj Y axis
+  YScale = d3.scaleLinear()
+  .domain([getMin(graphData), getMax(graphData)])
+  .range([heightReduced, 0 ]);
+  svg.selectAll("g.y.axis")
+  .call(d3.axisLeft(YScale));
+
+  var selection = svg.selectAll(".new_path")
+  .data(graphData)
+  .join(
+    function(enter){
+      return enter.append("path");
+    }
+    ,
+    function(update){ 
+      return update;
+    },
+    function(exit){
+      return exit.remove();
+    }
+
+  )
+  .attr("fill", "none")
+  .attr("class", "new_path")
+  .attr("stroke", function(d){return d[0][3]})
+  .attr("d", d3.line()
+    .curve(d3.curveLinear)
+    .x(function(d) { return XScale(d[0]) })
+    .y(function(d) { return YScale(d[1]) })
+  )
+  .lower();
+
+  for (i=0; i<graphData.length; i++){
+    svg.append('rect')
+      .attr('x', XScale((i+1)*30))
+      .attr('y', 0)
+      .attr('width', 1)
+      .attr('height', heightReduced)
+      .attr('stroke', 'black');
+  }
+
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function that get the data from source file (mySRC)
+// parse all data and put it in the output variable (myOUT)
+function getData(mySRC, myOUT){
+  return d3.csv(mySRC,
     // When reading the csv, I must format variables:
     function(d){
       var temp;
@@ -60,16 +144,66 @@ function getData(src){
         if (temp !== temp){
           temp = 0;
         }
-        DictData[j].push([date,temp]);
+        myOUT[j].push([date,temp]);
         temp=0;
-      }
-      //temp = new Date(d["date"]);
-      //DictData[countriesEurope.length].push(temp);
-    }
-  );  
+      }});  
 }
 
-// initialize the select elements with a list of names/texts and the id of the select element
+// function that take a liste of string containing dates "listDate"
+// an array of data contained in the file "countryData" probably a sub array of dictData
+// and retrun an output variable "out"
+function extractData(listDate, countryData, color, myOUT){
+  var currentMyOUTlength = myOUT.length;
+  for (q=0; q<listDate.length; q++){
+    myOUT.push([]);
+  }
+  //for each date in the list
+  for (d=0; d<listDate.length; d++){
+    console.log(d);
+    //get the indice in the data where we want to start plotting (date of implementatio of the policy)
+    var  indice = 0;
+    var begginingDate = new Date(listDate[d]);
+    for (i=0; i<countryData.length; i++){
+      if (begginingDate.getTime() === countryData[i][0].getTime()){
+        indice = i;
+      }
+    }
+    //get the 30 first element from the date of implementation of the policy
+    //normalize then to have 0=number of cases the first day
+    for (j=0; j<30; j++){
+      if (indice+j >= countryData.length) { break; }
+      myOUT[d+currentMyOUTlength].push(
+        [
+          30*d+j, 
+          100*(countryData[indice+j][1]-countryData[indice][1])/(countryData[indice][1]+1), 
+          countryData[indice+j][0],
+          color
+        ]);
+    }
+  }
+  return myOUT;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Functions to initiate and handle the html "select" elements ||||||||||||||||||||||||||
+
+// initialize the select elements with a list of names/texts 
+//and the id of the select element
 function initSelect(data, selectId, value){
   var str = "";
   var i = 0;
@@ -90,279 +224,45 @@ function selectOnChange(selectID){
   }if (selectID=="selectFactor"){
     parameter['factor'] = document.getElementById(selectID).value;
   }console.log("parameters : " +parameter['country'] +' '+ parameter['policy'] +' '+ parameter['factor'])
-  updateSelect();
-  updateGraph();
+
+  extractedData = [];
+  extractedData = extractData(startDate, dictData[parameter['country']], "red", extractedData);
+  updateALL(extractedData);
+}
+
+// Add a country to the Graph 3 to compare with the first one
+function selectOnChangeG3(selectID){
+  extractedData = [];
+  extractedData = extractData(startDate, dictData[parameter['country']], "red", extractedData);
+  extractedData = extractData(startDate, dictData[document.getElementById(selectID).value], "red", extractedData);
+  updateALL(extractedData);
 }
 
 
+//MATH FUNCTIONS |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-
-function updateSelect() {
-  // maj X axis 
-  XScale = d3.scaleTime()
-  .domain([new Date('2020-01-22'), new Date('2022-01-13')])
-  .range([ 0, widthReduced ]);
-  svg.selectAll("g.x.axis")
-  .attr("transform", `translate(0, ${heightReduced})`)
-  .call(d3.axisBottom(XScale));
-
-  // maj Y axis
-  YRangeMin = Math.min(...transpose(DictData[parameter['country']])[1]);
-  YRangeMax = Math.max(...transpose(DictData[parameter['country']])[1]);
-  YScale = d3.scaleLinear()
-  .domain([YRangeMin, YRangeMax])
-  .range([heightReduced, 0 ]);
-  svg.selectAll("g.y.axis")
-  .call(d3.axisLeft(YScale));
-}
-
-function updateGraph(){
-  var selection = svg.selectAll("new_path")
-  .data([DictData[parameter['country']]])
-  .join(
-    function(enter){
-      return enter.append("path");
-    }
-    ,
-    function(update){
-      return update;
-    },
-    function(exit){
-      return exit.remove();
-    }
-
-  )
-  .attr("fill", "none")
-  .attr("stroke", "blue")
-  .attr("d", d3.line()
-    .curve(d3.curveLinear)
-    .x(function(d) { return XScale(d[0]) })
-    .y(function(d) { return YScale(d[1]) })
-  )
-  .lower();
-}
- 
-
-
-
-
-
+// 2D matrix transpose
 function transpose(array2D){
   return array2D[0].map((_, colIndex) => array2D.map(row => row[colIndex]));
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
-
-// set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 30, left: 60},
-width1 = 800 - margin.left - margin.right,
-width2 = 800,
-height1 = 400 - margin.top - margin.bottom;
-height2 = 400;
-
-// create tooltip and circle for tooltip
-var ttip = d3.select("body").append("div")
-.attr("class", "tooltip")
-.style("position", 'absolute')
-.style("text-align", 'center')
-.style("width", '110px')
-.style("height", '40px')
-.style("padding", '2px')
-.style("border-radius", '8px')
-.style("font", '12px sans-serif')
-.style("background", 'lightsteelblue')
-.style("border", '0px')
-.style("pointer-events", 'none')
-.style("opacity", 0);
-var crcl = d3.select("body").append("div")
-.attr("class", "circle")
-.style("opacity", 0);
-
-// append the svg object to the body of the page
-const svg = d3.select("#third-graph")
-.append("svg")
-.attr("viewBox", `0 0 `+width2 +` `+height2)
-.append("g")
-.attr("transform", `translate(${margin.left},${margin.top})`);
-//.attr("width", width + margin.left + margin.right)
-//.attr("height", height + margin.top + margin.bottom)
-
-
-
-
-// load the first set of data when load page
-window.onload = function() {
-  initPage("data/new_cases_per_million.csv");
-  updateSelect("data/new_cases_per_million.csv");
-};
-
-
-
-
-// function that get the data and parse it
-function getData(val){
-  return d3.csv(val,
-    // When reading the csv, I must format variables:
-    function(d){
-      //console.log(d);
-      return { 
-        date : d3.timeParse("%Y-%m-%d")(d.date), 
-        valueTmed : parseFloat(d.France) }
-      }
-      );
+// min of a 3D matrix only on the 2nd element the 3rd dim 
+// ie : min for all row, for all col, for all 2nd elem
+function getMin(arrayData){
+  var temp = [];
+  for (i=0; i<arrayData.length; i++){
+    temp.push(Math.min(...transpose(arrayData[i])[1]));
+  }
+  return Math.min(...temp);
 }
 
-// function that create the X Axix
-function getXAxis(theData, width){
-  var x = d3.scaleTime()
-  .domain(d3.extent(theData, function(d) { return d.date; }))
-  .range([ 0, width1 ]);
-  return x;
-}
-function getYAxis(theData, height){
-  var y = d3.scaleLinear()
-  .domain([0, d3.max(theData, function(d) { return +d.valueTmed; })])
-  .range([ height1, 0 ]);
+// max of a 3D matrix only on the 2nd element the 3rd dim 
+// ie : max for all row, for all col, for all 2nd elem
+function getMax(arrayData){
+  var temp = [];
+  for (i=0; i<arrayData.length; i++){
+    temp.push(Math.max(...transpose(arrayData[i])[1]));
+  }
+  return Math.max(...temp);
 }
 
-function initPage(val){
-  var theData = getData(val);
-  theData.then(
-    function(data) {
-    // Add X axis 
-    svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", `translate(0, ${height1})`);
-    // Add Y axis
-    svg.append("g")
-    .attr("class", "y axis");
-    // Add the line
-    svg.append("path")
-    .attr("class", "dataLine");
-    // Add the line
-    svg.append("circle")
-    .attr("class", "dot");
-    // Add the line
-    svg.append("rect")
-    .attr("class", "hitbox");
-  })
-}
-
-function updateSelect(val) {
-  //clear svg
-  d3.select("path").remove();
-  svg.selectAll("circle").remove();
-  svg.selectAll("rect").remove();
-
-  //Read the data
-  var theData = getData(val);
-  console.log(theData);
-
-  theData.then(
-  // Now I can use this dataset:
-  function(data) {
-    // maj X axis 
-    var x = d3.scaleTime()
-    .domain(d3.extent(data, function(d) { return d.date; }))
-    .range([ 0, width1 ]);
-    svg.selectAll("g.x.axis")
-    .attr("transform", `translate(0, ${height1})`)
-    .call(d3.axisBottom(x));
-
-    // maj Y axis
-    var y = d3.scaleLinear()
-    .domain([0,d3.max(data, function(d) { return +d.valueTmed; })])
-    .range([ height1, 0 ]);
-    svg.selectAll("g.y.axis")
-    .call(d3.axisLeft(y));
-
-    // maj of the line
-    svg.selectAll("path.dataLine")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr("d", d3.line()
-      .x(function(d) { return x(d.date) })
-      .y(function(d) { return y(d.valueTmed) })
-      )
-    // add the hitbox with tooltips
-    svg.selectAll("rect.hitbox")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr('x', function(d) { return x(d.date); })
-    .attr('y', 0)
-    .attr('height', function(d) { return height1; })
-    .attr('width', function(d) { return width1/364; })
-    .attr('fill', "#0000")
-    .attr('stroke', "none")
-    .on("mouseover", function(event,d) {
-      svg.append('circle')
-      .attr('cx', x(d.date))
-      .attr('cy', y(d.valueTmed))
-      .attr('r', 5)
-      .attr('stroke', 'black')
-      .attr('fill', 'black');
-      ttip.transition()
-      .duration(0)
-      .style("opacity", .9);
-      ttip.html(
-        "Date: " + 
-        d.date.toLocaleString('default', { month: 'long' })  + 
-        " " + 
-        d.date.getUTCDate() + 
-        "<br/>Tmin: " + 
-        d.valueTmin + 
-        "<br/>Tmax: " + 
-        d.valueTmax
-        )
-      .style("left", (event.pageX) + "px")
-      .style("top", (event.pageY - 28) + "px");
-    })
-    .on("mouseout", function(d) {
-     svg.selectAll("circle").remove();
-     ttip.transition()
-     .duration(0)
-     .style("opacity", 0);
-   });
-  })
-}
-
-**/
